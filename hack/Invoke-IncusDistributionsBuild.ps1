1a56c14d9fec809b1b8aced75af4ecec77fd0ca0
Antoine Martin <antoine@mrtn.fr>
2025-08-18 11:51:21 +0200

    ✨ Script and gha workflow to pre-compute incus root filesystems

---
+++ hack/Invoke-IncusDistributionsBuild.ps1
@@ -0,0 +1,92 @@
+# Takes as a parameter the output file
+[CmdletBinding(SupportsShouldProcess = $true)]
+param(
+    [Parameter(Mandatory = $true)]
+    [string]$OutputFile
+)
+
+# Some constants
+$IncusImageStreamUrl = "https://images.linuxcontainers.org/streams/v1/index.json"
+$IncusBaseImageUrl = "https://images.linuxcontainers.org/images"
+$IncusDirectorySuffix = "amd64/default"
+$IncusRootfsName = "rootfs.tar.xz"
+
+function Emoji {
+    param (
+        [string]$code
+    )
+    $EmojiIcon = [System.Convert]::toInt32($code, 16)
+    return [System.Char]::ConvertFromUtf32($EmojiIcon)
+}
+
+$script:HourGlass = Emoji "231B"
+$script:PartyPopper = Emoji "1F389"
+$script:Eyes = Emoji "1F440"
+
+function Progress {
+    param (
+        [string]$message
+    )
+    Write-Host "$script:HourGlass " -NoNewline
+    Write-Host -ForegroundColor DarkGray $message
+}
+
+function Success {
+    param (
+        [string]$message
+    )
+    Write-Host "$script:PartyPopper " -NoNewline
+    Write-Host -ForegroundColor DarkGreen $message
+}
+
+function Information {
+    param (
+        [string]$message
+    )
+    Write-Host "$script:Eyes " -NoNewline
+    Write-Host -ForegroundColor DarkYellow $message
+}
+
+
+# Combination of the two preceding functions that return a WslRootFileSystem
+# for all incus distributions
+function Get-AllIncusRootFileSystems {
+    process {
+        Progress "Processing all incus filesystems"
+        Invoke-RestMethod $IncusImageStreamUrl |
+            ForEach-Object { $_.index.images.products } | Select-String 'amd64:default$' |
+            ForEach-Object { $_ -replace '^(?<distro>[^:]+):(?<release>[^:]+):.*', '${distro},"${release}"' } |
+            ConvertFrom-Csv -Header Os, Release |
+            ForEach-Object {
+                Progress "Processing $($_.Os) $($_.Release)"
+                $url = "$IncusBaseImageUrl/$($_.Os)/$($_.Release)/$IncusDirectorySuffix"
+                $last_release_directory = try {
+                    (Invoke-WebRequest $url).Links | Select-Object -Last 1 -ExpandProperty "href"
+                }
+                catch {
+                    "unknown/"
+                }
+                $Name = (Get-Culture).TextInfo.ToTitleCase($_.Os)
+                $Uri = [System.Uri]"$url/$last_release_directory$IncusRootfsName"
+
+                return [PSCustomObject]@{
+                        Type = "Incus"
+                        Os = $_.Os
+                        Name = $Name
+                        Username = 'root'
+                        Uid = 0
+                        Release = $_.Release
+                        Url = $Uri.ToString()
+                        Configured = $false
+                        LocalFileName = "incus.$($_.Os)_$($_.Release).rootfs.tar.gz"
+                        Hash = [PSCustomObject]@{
+                            Url       = [System.Uri]::new($Uri, "SHA256SUMS").ToString()
+                            Type      = 'sums'
+                            Algorithm = 'SHA256'
+                        }
+                }
+            }
+    }
+}
+
+Get-AllIncusRootFileSystems | ConvertTo-Json -Depth 5 | Out-File -FilePath $OutputFile -Encoding utf8 -Force
